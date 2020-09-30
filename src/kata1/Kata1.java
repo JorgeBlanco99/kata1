10b30e8c8a5120f10a8442b63e6e78b5759448e1
jorge <jorge@localhost>
2020-09-30 14:08:20 +0100

    final project

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,5 +1,8 @@
 package kata1;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  *
  * @author jorge
@@ -7,7 +10,8 @@ package kata1;
 public class Kata1 {
 
     public static void main(String[] args) {
-        
+        Person person = new Person ("Jorge Blanco", LocalDate.of(1999,Month.DECEMBER,20));
+        System.out.println(person.getName() +" " + person.getAge());
     }
     
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,10 +1,32 @@
 
 package kata1;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author jorge
  */
 public class Person {
+    private final String name;
+    private final LocalDate birthdate;
 
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+   public int getAge(){
+       return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+   }
+   public int toYears(long days){
+       return (int) (days/365);
+   }
 }
